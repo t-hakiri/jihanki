beffecce2c85d3a372cda8081213361b821a7989
takuya hakiri <tyapi4@yahoo.co.jp>
2019-11-29 14:52:59 +0900

    add:基本機能

---
+++ training.rb
@@ -0,0 +1,96 @@
+# このコードをコピペしてrubyファイルに貼り付け、そのファイルをirbでrequireして実行しましょう。
+
+# 例
+
+# irb
+# require '/Users/shibatadaiki/work_shiba/full_stack/sample.rb'
+# （↑のパスは、自動販売機ファイルが入っているパスを指定する）
+
+# 初期設定（自動販売機インスタンスを作成して、vmという変数に代入する）
+# vm = VendingMachine.new
+
+# 作成した自動販売機に100円を入れる
+# vm.slot_money (100)
+
+# 作成した自動販売機に入れたお金がいくらかを確認する（表示する）
+# vm.current_slot_money
+
+# 作成した自動販売機に入れたお金を返してもらう
+# vm.return_money
+
+class VendingMachine
+  # ステップ０　お金の投入と払い戻しの例コード
+  # ステップ１　扱えないお金の例コード
+
+  # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
+  MONEY = [10, 50, 100, 500, 1000].freeze
+  attr_accessor :juice, :sale
+
+  # （自動販売機に投入された金額をインスタンス変数の @slot_money に代入する）
+  def initialize
+    # 最初の自動販売機に入っている金額は0円
+    @slot_money = 0
+    @juice = Juice.new
+    @sale = 0
+    puts @juice.name
+  end
+
+  # 投入金額の総計を取得できる。
+  def current_slot_money
+    # 自動販売機に入っているお金を表示する
+    @slot_money
+  end
+
+  # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
+  # 投入は複数回できる。
+  def slot_money(money)
+    # 想定外のもの（１円玉や５円玉。千円札以外のお札、そもそもお金じゃないもの（数字以外のもの）など）
+    # が投入された場合は、投入金額に加算せず、それをそのまま釣り銭としてユーザに出力する。
+    return false unless MONEY.include?(money)
+    # 自動販売機にお金を入れる
+    @slot_money += money
+  end
+
+  def buy_juice
+    if @juice.number >= 1 && current_slot_money >= 120
+      puts 'コーラが現れた'
+      @sale = @juice.price
+      @juice.number -= 1
+      @slot_money -= @juice.price
+      return_money
+    end
+  end
+
+  # 払い戻し操作を行うと、投入金額の総計を釣り銭として出力する。
+  def return_money
+    # 返すお金の金額を表示する
+    puts "#{@slot_money}円のお釣りです"
+    # 自動販売機に入っているお金を0円に戻す
+    @slot_money = 0
+  end
+end
+
+class Juice
+  attr_accessor :name, :price, :number
+  def initialize
+    @name = 'cola'
+    @price = 120
+    @number = 5
+  end
+
+  # def make
+  #   @juice = Juice.new
+  # end
+end
+
+class User
+end
+
+@vend = VendingMachine.new
+@vend.slot_money(100)
+@vend.slot_money(10)
+@vend.slot_money(10)
+@vend.slot_money(10)
+@vend.buy_juice
+puts @vend.sale
+puts @vend.juice.number
